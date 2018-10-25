64027cb192b455e692d024d78e89cfafb26e3bbd
Cory Kim <corykim0829@gmail.com>
2018-10-25 20:31:49 +0900

    2

--- src/main/StringAddCalculator.java
+++ src/main/StringAddCalculator.java
@@ -7,7 +7,6 @@ public class StringAddCalculator {
 
         if(isNullOrEmpty(text)) { return 0; }
 
-<<<<<<< HEAD
         int sum = 0;
         for (String token : splitText(text)) {
             checkException(token);
@@ -17,17 +16,11 @@ public class StringAddCalculator {
     }
 
     private String[] splitText(String text) {
-=======
-        return checkRegex(text);
-    }
 
-    private int checkRegex(String text) {
->>>>>>> 9320ff2fc5ae48fd2d4ae57bec53740b9de73ec5
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
         if(m.find()) {
             String customDelimiter = m.group(1);
             String[] tokens = m.group(2).split(customDelimiter);
-<<<<<<< HEAD
             return tokens;
         }
         String[] tokens = text.split(",|:");
@@ -36,29 +29,6 @@ public class StringAddCalculator {
 
     public boolean isNullOrEmpty(String text) {
         return (text == null || text.isEmpty());
-=======
-            return sum(tokens);
-        }
-
-        String[] tokens = text.split(",|:");
-        return sum(tokens);
-    }
-
-    public boolean isNullOrEmpty(String text) {
-        if(text == null || text.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
-
-    public int sum(String[] tokens) {
-        int sum = 0;
-        for (int i = 0; i < tokens.length; i++) {
-            checkException(tokens[i]);
-            sum += Integer.parseInt(tokens[i]);
-        }
-        return sum;
->>>>>>> 9320ff2fc5ae48fd2d4ae57bec53740b9de73ec5
     }
 
     public void checkException(String token) {
